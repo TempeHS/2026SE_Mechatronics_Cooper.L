327143ef7e807f41e94e067ba2195ff34a8b47de
Cooper L <55725860+XCoop@users.noreply.github.com>
2025-08-21 12:47:18 +1000

    display?

--- project/py_scripts/display.py
+++ project/py_scripts/display.py
@@ -1,15 +1,22 @@
 from PiicoDev_SSD1306 import *
 from PiicoDev_Unified import sleep_ms
 from PiicoDev_VEML6040 import PiicoDev_VEML6040
-from coloursensor import Scan
+from coloursensor import ColourScan
 
-colourSensor = PiicoDev_VEML6040()
-display = create_PiicoDev_SSD1306()
-data = Scan(colourSensor)
 
 
 while True:
     display.fill(0)
-    display.text("PiicoDev",30,20, 1)
-    display.text(str(data.scan()),50,35, 1)
+    display.text(str(data.ColourScan()),50,35, 1)
     display.show()
+
+class Display:
+    def init__(self, display, debug):
+        self.__display = display
+    def display(self, text):
+        display.fill(0)
+        display.text(str(text), 50, 35, 1)
+        display.show()
+        
+
+        
